1200dee5b45f3094f5dd6db715859a4770edfd30
grzlz <guillermo.rdzlpz@gmail.com>
2025-08-09 19:28:07 -0600

    Automatic deploy

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script>
 	import '../app.css';
-	import  injectAnalytics  from '@vercel/analytics/sveltekit';
+	import  { injectAnalytics }  from '@vercel/analytics/sveltekit';
 
 	injectAnalytics()
 
