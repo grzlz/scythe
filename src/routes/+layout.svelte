dc24c66110f1adcf01a4d2e54ed9eafd00ae382d
grzlz <guillermo.rdzlpz@gmail.com>
2025-08-14 08:45:51 -0600

    Automatic deploy

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,7 +17,7 @@
 <nav class="flex justify-between items-center p-2 fixed top-0 left-0 right-0 z-10 backdrop-blur-xs">
 	<!-- Logo - Left -->
 	<div class="flex items-center relative gap-1 top-0.5">
-		<img src="/logo.png" alt="Scythe" class="h-5 w-auto" />
+		<img src="/logo.png" alt="Scythe" class="h-8 w-auto" />
 		<span class="text-white font-primary relative top-0.5" style="font-size: 20px; line-height: 20px;">SCYTHE</span>
 	</div>
 	
